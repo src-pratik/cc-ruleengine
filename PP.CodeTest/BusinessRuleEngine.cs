ec3569e31a0c49615f710f191c3833a9a59f5a70
Pratik Patil <connect@pratikpatil.net>
2020-10-08 22:15:28 +0530

    Implemented Business Rule Engine

---
+++ PP.CodeTest/BusinessRuleEngine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PP.CodeTest
+{
+    /// <summary>
+    /// Engine to verify the business rules against an given input.
+    /// </summary>
+    public class BusinessRuleEngine : IRuleEngine, IBusinessRulesEngine
+    {
+        /// <summary>
+        /// Name for the engine
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// List of all rules that are defined
+        /// </summary>
+        public List<BusinessFunctionRule> Rules { get; }
+
+        public BusinessRuleEngine(string name, List<BusinessFunctionRule> rules)
+        {
+            Name = name;
+            Rules = rules.OrderBy(x => x.Order).ToList();
+        }
+
+        /// <summary>
+        /// Process the rules on the input
+        /// </summary>
+        /// <param name="item">Input on which the rules are to be applied</param>
+        /// <param name="onRuleHit">Invoke delegate when a particular rules is satisified</param>
+        /// <param name="onComplete">Invoke delegate when the engine has completely processed the input</param>
+        public void Process(OrderItem item, Action<BusinessFunctionRule, OrderItem> onRuleHit = null, Action onComplete = null)
+        {
+            Rules.ForEach(r =>
+            {
+                if (r.WhereCondition(item))
+                    onRuleHit?.Invoke(r, item);
+            });
+
+            onComplete?.Invoke();
+        }
+    }
+}
