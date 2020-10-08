2d16f4fc4535ced482b63a165f35d243bc661027
Pratik Patil <connect@pratikpatil.net>
2020-10-08 23:20:10 +0530

    Added Mock Price Store & Added Trace mechanism for testing

---
+++ PP.CodeTest/PromotionEngine/Pricing.cs
@@ -0,0 +1,47 @@
+﻿namespace PP.CodeTest
+{
+    /// <summary>
+    /// Mock pricing store to get pricing information
+    /// </summary>
+    public static class Pricing
+    {
+        public static double Price(string key)
+        {
+            switch (key)
+            {
+                case "A":
+                    return 50;
+
+                case "B":
+                    return 30;
+
+                case "C":
+                    return 20;
+
+                case "D":
+                    return 10;
+
+                default:
+                    return 0;
+            }
+        }
+        public static double PromoPrice(string promoKey)
+        {
+            switch (promoKey)
+            {
+                case "3 Of A`s":
+                    return 130;
+
+                case "2 Of B`s":
+                    return 45;
+
+                case "C & D":
+                    return 30;
+
+                default:
+                    return 0;
+            }
+        }
+    }
+
+}
