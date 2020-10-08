ab7eda71ad8f7d87c69fd5ebf520c0f0630cfba9
Pratik Patil <connect@pratikpatil.net>
2020-10-08 22:09:48 +0530

    Interface for Business Rules Engine

---
+++ PP.CodeTest/IBusinessRulesEngine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PP.CodeTest
+{
+    /// <summary>
+    /// Interface for Business Engine
+    /// </summary>
+    public interface IBusinessRulesEngine
+    {
+        List<BusinessFunctionRule> Rules { get; }
+
+        void Process(OrderItem item, Action<BusinessFunctionRule, OrderItem> onRuleHit = null, Action onComplete = null);
+    }
+}
