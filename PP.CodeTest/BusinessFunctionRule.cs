617d9d909a8643064411dade0ea99a8617d57468
Pratik Patil <connect@pratikpatil.net>
2020-10-08 22:07:42 +0530

    Business Function Rule Definition

---
+++ PP.CodeTest/BusinessFunctionRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PP.CodeTest
+{
+    /// <summary>
+    /// Business Rule which stores the business rules to be verified if they are satisfied
+    /// </summary>
+    public class BusinessFunctionRule : IRule
+    {
+        /// <summary>
+        /// Code to identify the rule
+        /// </summary>
+        public string Code { get; }
+
+        /// <summary>
+        /// Order in which the rule needs to be executed.
+        /// This can be helpful when we need to carry out rules & actions in dependent sequence
+        /// </summary>
+        public int Order { get; }
+
+        /// <summary>
+        /// The where condition which defines the business rule to be verified.
+        /// </summary>
+        public Func<OrderItem, bool> WhereCondition { get; }
+
+        public BusinessFunctionRule(string code, Func<OrderItem, bool> whereCondition, int order = 0)
+        {
+            WhereCondition = whereCondition;
+            Code = code;
+        }
+    }
+}
--- PP.CodeTest/OrderItem.cs
+++ PP.CodeTest/OrderItem.cs
@@ -9,5 +9,4 @@
         public ProductTypeEnum ProductType { get; set; }
         public DeliveryTypeEnum DeliveryType { get; set; }
     }
-
 }
