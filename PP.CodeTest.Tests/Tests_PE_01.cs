c9b11d89b9a66975010b86f7d76dad74f5aad824
Pratik Patil <connect@pratikpatil.net>
2020-10-08 23:32:09 +0530

    Added Some Test Cases to verify logic
    Test_PromoHit_A_OneTime,
    Test_PromoHit_A_3Time,
    Test_PromoHit_A1Time_B2Time,
    Test_PromoHit_CDOneTime

--- PP.CodeTest.Tests/Tests_PE_01.cs
+++ PP.CodeTest.Tests/Tests_PE_01.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -33,6 +34,80 @@ namespace PP.CodeTest.Tests
 
             Assert.Empty(log.Details);
             Assert.Equal(Pricing.Price("A") + Pricing.Price("B") + Pricing.Price("C"), log.TotalAmount);
+        }
+        
+        [Fact]
+        public void Test_PromoHit_A_OneTime()
+        {
+            List<CartItem> shoppingCart = new List<CartItem> {
+                new CartItem {  Key = "A", Count = 3 },
+                new CartItem {  Key = "B", Count = 0 },
+                new CartItem {  Key = "C", Count = 0 },
+                new CartItem {  Key = "D", Count = 0 }
+            };
+
+            var log = new PromoEngineEventLogTrace(shoppingCart);
+            promotionRuleEngine.Process(shoppingCart, log.HandlePromoDiscount, log.CalculateTotal);
+
+            Assert.True(log.Details.Values.Where(x => x.Item1 == A3).Any());
+            Assert.Single(log.Details);
+            Assert.Equal(Pricing.PromoPrice(A3), log.TotalAmount);
+        }
+        [Fact]
+        public void Test_PromoHit_A_3Time()
+        {
+            List<CartItem> shoppingCart = new List<CartItem> {
+                new CartItem {  Key = "A", Count = 10 },
+                new CartItem {  Key = "B", Count = 0 },
+                new CartItem {  Key = "C", Count = 0 },
+                new CartItem {  Key = "D", Count = 0 }
+            };
+
+            var log = new PromoEngineEventLogTrace(shoppingCart);
+            promotionRuleEngine.Process(shoppingCart, log.HandlePromoDiscount, log.CalculateTotal);
+            Assert.True(log.Details.Values.Where(x => x.Item1 == A3).Count() == 3);
+            Assert.Equal(3 * Pricing.PromoPrice(A3) + Pricing.Price("A"), log.TotalAmount);
+        }
+
+
+        [Fact]
+        public void Test_PromoHit_A1Time_B2Time()
+        {
+            List<CartItem> shoppingCart = new List<CartItem> {
+                new CartItem {  Key = "A", Count = 3 },
+                new CartItem {  Key = "B", Count = 5 },
+                new CartItem {  Key = "C", Count = 0 },
+                new CartItem {  Key = "D", Count = 0 }
+            };
+
+            var log = new PromoEngineEventLogTrace(shoppingCart);
+            promotionRuleEngine.Process(shoppingCart, log.HandlePromoDiscount, log.CalculateTotal);
+
+            Assert.True(log.Details.Values.Where(x => x.Item1 == A3).Count() == 1);
+            Assert.True(log.Details.Values.Where(x => x.Item1 == B2).Count() == 2);
+
+            Assert.Equal(3, log.Details.Count);
+            Assert.Equal(1 * Pricing.PromoPrice(A3) + 2 * Pricing.PromoPrice(B2) + Pricing.Price("B"), log.TotalAmount);
+
+        }
+
+        [Fact]
+        public void Test_PromoHit_CDOneTime()
+        {
+            List<CartItem> shoppingCart = new List<CartItem> {
+                new CartItem {  Key = "A", Count = 0 },
+                new CartItem {  Key = "B", Count = 0 },
+                new CartItem {  Key = "C", Count = 1 },
+                new CartItem {  Key = "D", Count = 1 }
+            };
+
+            var log = new PromoEngineEventLogTrace(shoppingCart);
+            promotionRuleEngine.Process(shoppingCart, log.HandlePromoDiscount, log.CalculateTotal);
+
+            Assert.True(log.Details.Values.Where(x => x.Item1 == C1D1).Any());
+            Assert.Single(log.Details);
+            Assert.Equal(Pricing.PromoPrice(C1D1), log.TotalAmount);
+
         }
     }
 }
